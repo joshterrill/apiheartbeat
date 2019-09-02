e3db78f8712d768e235f1e1845a1d646f8a8d67e
Josh Terrill <joshterrill.dev@gmail.com>
2019-09-02 00:54:30 -0700

    moved angular build deps to deps

--- ci.sh
+++ ci.sh
@@ -1,7 +1,3 @@
 cd src/public/client
 npm ci
-ls node_modules/
-ls node_modules/@angular/
-ls node_modules/@angular/cli/
-ls node_modules/@angular/cli/bin/
-node_modules/@angular/cli/bin/ng build --configuration=production --output-path ../compiled/
+ng build --configuration=production --output-path ../compiled/
