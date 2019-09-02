f3e3683cab102c56384d992be987fada132b58a8
Josh Terrill <joshterrill.dev@gmail.com>
2019-09-02 00:51:19 -0700

    testing ls output for heroku build

--- ci.sh
+++ ci.sh
@@ -1,3 +1,11 @@
 cd src/public/client
 npm ci
+echo "1"
+ls ./
+echo "2"
+ls ../
+echo "3"
+ls ../../
+echo "4"
+ls ../../../
 ./node_modules/@angular/cli/bin/ng build --configuration=production --output-path ../compiled/
