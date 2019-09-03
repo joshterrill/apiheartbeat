d127b7352546b84123226cd1e03d4200251e6afb
Josh Terrill <joshterrill.dev@gmail.com>
2019-09-02 20:45:16 -0700

    updated ci script for heroku

--- ci.sh
+++ ci.sh
@@ -1,5 +1,7 @@
 cd src/public/client
 npm ci
+npm i --only=dev
+npm update
 npm i -g @angular/cli
 npm i @angular-devkit/build-angular
 ng build --configuration=production --output-path ../compiled/
